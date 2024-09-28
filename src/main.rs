41429358e9a6fd24c3976da5daff2d303995a951
chinmay <chinmay.khadilkar@gmail.com>
2024-09-28 16:55:21 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-const commands = Vec!["echo", "type", "exit"];
+const commands:Vec<str> = Vec!["echo", "type", "exit"];
 fn main() {
     // Uncomment this block to pass the first stage
+    let commands: Vec<String> = vec!["echo".to_string(), "type".to_string(), "exit".to_string()];
     loop {
         print!("$ ");
         
@@ -12,17 +13,20 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+        let cmd_args = input.trim().split(" ").collect::<Vec<&str>>();
+        let cmd = cmd_args[0];
+        let args = cmd_args[1..].join(" ");
         if input.trim() == String::from("exit 0") {
             break;
-        } else if input.trim().contains("echo") {
-            println!("{}", input.trim().split(" ").collect::<Vec<&str>>()[1..].join(" "));
-        } else if input.trim().contains("type") {
-            let cmd = input.trim().split(" ").collect::<Vec<&str>>()[1..].join("");
-            if  commands.contains(cmd) {
-                println!("{} is a builtin command", cmd);
+        }  else if cmd == String::from("type") {
+            // let query_cmd = args.join("");
+            if  commands.contains(&args) {
+                println!("{} is a builtin command", args);
             } else {
-                println!("{}: command not found", input.trim());
+                println!("{}: command not found", args);
             }
+        } else if cmd == String::from("echo") {
+            println!("{}", args);
         } else {
             println!("{}: command not found", input.trim());
         }
