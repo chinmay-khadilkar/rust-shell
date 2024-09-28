805f4663998f9aff8a4cf9d36086b5bef4286f20
chinmay <chinmay.khadilkar@gmail.com>
2024-09-28 20:23:43 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
         } else if cmd == String::from("type") {
             // let query_cmd = args.join("");
             let path: Vec<&str> = raw_path.split(":").collect::<Vec<&str>>();
-            let path_index = raw_path.iter().position(|&r| r.contains(&args));
+            let path_index = path.iter().position(|&r| r.contains(&args));
             match path_index {
                 Some(pos) => println!("{} is {}", args, path[pos]),
                 None => println!("{}: not found", args)
